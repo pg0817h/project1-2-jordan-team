1dd73bd7184957033329a24630e72724746abb89
pg0817h <31809795+pg0817h@users.noreply.github.com>
2017-09-25 19:48:09 -0700

    Update PriceList.cpp

--- PriceList.cpp
+++ PriceList.cpp
@@ -1,3 +1,4 @@
+
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -9,36 +10,97 @@ using namespace std;
 
 // Load information from a text file with the given filename.
 void PriceList::createPriceListFromDatafile(string filename) {
-	ifstream myfile(filename);
-
-	if (myfile.is_open()) {
-		cout << "Successfully opened file " << filename << endl;
-		string name;
-		string code;
-		double price;
-		bool taxable;
-		while (myfile >> name >> code >> price >> taxable) {
-			// cout << code << " " << taxable << endl;
-			addEntry(name, code, price, taxable);
-		}
-		myfile.close();
-	}
-	else
-		throw invalid_argument("Could not open file " + filename);
+    ifstream myfile(filename);
+    
+    if (myfile.is_open()) {
+        cout << "Successfully opened file " << filename << endl;
+        string name;
+        string code;
+        double price;
+        bool taxable;
+        while (myfile >> name >> code >> price >> taxable) {
+            // cout << code << " " << taxable << endl;
+            addEntry(name, code, price, taxable);
+        }
+        myfile.close();
+    }
+    else
+        throw invalid_argument("Could not open file " + filename);
 }
 
 // return true only if the code is valid
 bool PriceList::isValid(string code) const {
-	// TO BE COMPLETED
-
+    // TO BE COMPLETED
+    PriceListItem *temp = itemList;
+    if(temp != NULL)
+    {
+        if(temp->getCode().compare(code)==0)
+        {
+            return true;
+        }
+        while(temp->nextItem != NULL)
+        {
+            if(temp->getCode().compare(code)==0)
+            {
+                return true;
+            }
+            temp = temp->nextItem;
+        }
+    }
+    return false;
 }
 
 // return price, item name, taxable? as an ItemPrice object; throw exception if code is not found
 PriceListItem PriceList::getItem(string code) const {
-	// TO BE COMPLETED
+    // TO BE COMPLETED
+    if(isValid(code))
+    {
+        PriceListItem *temp = itemList;
+        if(temp != NULL)
+        {
+            if(temp->getCode().compare(code)==0)
+            {
+                return *temp;
+            }
+            while(temp->nextItem != NULL)
+            {
+                if(temp->getCode().compare(code)==0)
+                {
+                    return *temp;
+                }
+                temp = temp->nextItem;
+            }
+        }
+    }
+    throw invalid_argument("invalid code" + code);
+    
 }
 
 // add to the price list information about a new item
 void PriceList::addEntry(string itemName, string code, double price, bool taxable) {
-	// TO BE COMPLETED
+    // TO BE COMPLETED
+    //create an item
+    PriceListItem newItem(itemName,code,price,taxable);
+    //make item next to NULL
+    newItem.nextItem =NULL;
+    
+    PriceListItem *temp = itemList;
+    if(temp != NULL)
+    {
+        while(temp->nextItem != NULL)
+        {
+            temp = temp->nextItem;
+        }
+        
+        temp->nextItem = &newItem;
+    }
+    else
+    {
+        itemList = &newItem;
+    }
+    
+}
+PriceListItem* PriceList::getPriceList()
+{
+    return itemList;
 }
