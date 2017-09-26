454ebe619e58e51be5f3cc5de510ae576367e261
pg0817h <31809795+pg0817h@users.noreply.github.com>
2017-09-25 19:40:18 -0700

    Update PriceListItem.cpp

--- PriceListItem.cpp
+++ PriceListItem.cpp
@@ -1,28 +1,44 @@
+#include <fstream>
 #include "PriceListItem.h"
 
 PriceListItem::PriceListItem(const string &itemName, const string &code, double price, bool taxable) {
-	// TO BE COMPLETED
-	// added line
+    // TO BE COMPLETED
+    // added line
+  
+    this->itemName = itemName;
+    this->itemCode = code;
+    this->itemPrice = price;
+    this->isItemTaxable = taxable;
 }
 
 PriceListItem::PriceListItem() {
-	// TO BE COMPLETED
-
+    // TO BE COMPLETED
+    itemName = "";
+    itemCode = "";
+    itemPrice = 0.0;
+    isItemTaxable = false;
+    
+    
 }
 
 string PriceListItem::getItemName() {
-	// TO BE COMPLETED
+    // TO BE COMPLETED
+    return itemName;
 }
 
 string PriceListItem::getCode() {
-	// TO BE COMPLETED
+    // TO BE COMPLETED
+    return itemCode;
+    
 }
 
 double PriceListItem::getPrice() {
-	// TO BE COMPLETED
+    // TO BE COMPLETED
+    return itemPrice;
 }
 
 bool PriceListItem::isTaxable() {
-	// TO BE COMPLETED
+    // TO BE COMPLETED
+    return isItemTaxable;
 }
 
