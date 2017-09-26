20d398d8069e5ff276ea62b8702e1734bc8fbfd1
pg0817h <31809795+pg0817h@users.noreply.github.com>
2017-09-25 19:39:21 -0700

    Update GroceryBill.h

--- GroceryBill.h
+++ GroceryBill.h
@@ -4,15 +4,22 @@
 
 class GroceryBill {
 public:
-	GroceryBill(const PriceList *priceList, double taxRate);
-	void scanItem(string scanCode, double quantity); // add item and quantity to bill; throw exception if item's code is not found in the pricelist
-	void scanItemsFromFile(string filename); // Scan multiple codes and quantities from the given text file
-	double getTotal(); // return the total cost of all items scanned
-	void printBill(); // Print the bill to cout. Each line contains the name of an item, total price, and the letter "T" if tax was addded.
-
+    GroceryBill(const PriceList *priceList, double taxRate);
+    void scanItem(string scanCode, double quantity); // add item and quantity to bill; throw exception if item's code is not found in the pricelist
+    void scanItemsFromFile(string filename); // Scan multiple codes and quantities from the given text file
+    double getTotal(); // return the total cost of all items scanned
+    void printBill(); // Print the bill to cout. Each line contains the name of an item, total price, and the letter "T" if tax was addded.
+    
 private:
-	// any private member variables and methods go here
-
-	// TO BE COMPLETED
-
+    // any private member variables and methods go here
+    
+    // TO BE COMPLETED
+    PriceList price_list;
+    PriceListItem *itemList;
+    double total;
+    double tax_rate;
+    
+    
+    
 };
+
