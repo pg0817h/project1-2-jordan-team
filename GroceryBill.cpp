639065982d63b0f29a3cbbd6a3e76233c85d8647
pg0817h <31809795+pg0817h@users.noreply.github.com>
2017-09-25 19:49:57 -0700

    Update GroceryBill.cpp

--- GroceryBill.cpp
+++ GroceryBill.cpp
@@ -1,16 +1,54 @@
 #include "GroceryBill.h"
+#include <string>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
 
 GroceryBill::GroceryBill(const PriceList *priceList, double taxRate) {
-	// To be completed
-
+    // To be completed
+    price_list = *priceList;
+    tax_rate = taxRate;
+    
 }
 
 
 void GroceryBill::scanItem(string scanCode, double quantity) {
-	// To be completed
+    // To be completed
+    if(price_list.isValid(scanCode))
+    {
+        PriceListItem newItem = price_list.getItem(scanCode);
+        
+        if(newItem.isTaxable())
+        {
+            total +=(quantity * newItem.getPrice()) +((quantity * newItem.getPrice())* tax_rate/100);
+        }
+        
+        else
+        {
+            total += (quantity * newItem.getPrice());
+        }
+        
+        price_list.isValid(scanCode);
+        PriceListItem *temp = itemList;
+        if(temp != NULL)
+        {
+            while(temp->nextItem != NULL)
+            {
+                temp = temp ->nextItem;
+            }
+            
+            temp->nextItem = &newItem;
+        }
+        
+        else{
+            itemList = &newItem;
+        }
+    }
+    else
+    {
+        throw invalid_argument("Item is not found" + scanCode);
+    }
 }
 
 // Scan multiple codes and quantities from the given text file
@@ -18,16 +56,36 @@ void GroceryBill::scanItem(string scanCode, double quantity) {
 // Example line from text file:
 // 15000000 1.5
 void GroceryBill::scanItemsFromFile(string filename) {
-	// To be completed
-	// HINT: Look at code in PriceList::createPriceListFromDatafile(string filename)
+    // To be completed
+    // HINT: Look at code in PriceList::createPriceListFromDatafile(string filename)
+    
+    ifstream myfile(filename);
+    
+    if(myfile.is_open()){
+        cout << "Opened file successfully" << filename <<endl;
+        
+        string code;
+        double quantity;
+        
+        while(myfile >> code >> quantity){
+            
+            scanItem(code, quantity);
+        }
+        myfile.close();
+    }
+    
+    else
+        throw invalid_argument("It could not open file" + filename);
+    
 }
 
 // return the total cost of all items scanned
 double GroceryBill::getTotal() {
-	// To be completed
+    // To be completed
+    return total;
 }
 
-// Print the bill to cout. Each line contains the name of an item, total price, and the letter "T" if tax was addded. 
+// Print the bill to cout. Each line contains the name of an item, total price, and the letter "T" if tax was addded.
 // The last line shows the total.
 // An example:
 //Plastic_Wrap	1.60547 T
@@ -37,7 +95,25 @@ double GroceryBill::getTotal() {
 //Potatoes_red	13.446
 //TOTAL 	28.3765
 void GroceryBill::printBill() {
-	// To be completed
-
+    // To be completed
+    
+    PriceListItem *temp = itemList;
+    
+    while(itemList->nextItem != NULL)
+    {
+        if(temp->isTaxable())
+        {
+            cout << temp->getItemName() << "\n"<<temp->getPrice() << "\n"<<temp->isTaxable() << endl;
+        }
+        else
+        {
+            cout << temp->getItemName() << "\n" << temp->getPrice() << "\n" << temp->isTaxable()<<endl;
+        
+        }
+        
+        temp = temp->nextItem;
+    }
+    
 }
+
 
